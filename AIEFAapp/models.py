f60ba231473db1e9b8b9cd0ef242c0141b659fd5
jaechizzle4 <112498033+juanchavez710@users.noreply.github.com>
2023-10-19 15:47:58 -0500

    started local branch

--- AIEFAapp/models.py
+++ AIEFAapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Stock(models.Model):
+    symbol = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    graph_data = models.TextField()  # You can store prediction data in JSON format or other suitable format
+
+    def __str__(self):
+        return self.symbol
